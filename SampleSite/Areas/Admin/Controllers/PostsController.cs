2714956b87a7e759752188c140a4ad797d72c528
Kalfira <zdegner@gmail.com>
2015-04-04 01:19:40 -0500

    Init AuthSetup

---
+++ SampleSite/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace SampleSite.Areas.Admin.Controllers
+{
+    [Authorize(Roles="admin")]
+    public class PostsController : Controller
+    {
+        // GET: Admin/Posts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SampleSite/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace SampleSite.Areas.Admin.Controllers
+{
+    [Authorize(Roles="admin")]
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
