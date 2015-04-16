87e8b0dfe564e6ec162c55eaae5dad1f61ada7e3
Kalfira <zdegner@gmail.com>
2015-04-16 11:34:59 -0500

    Ugly UGLY CSS update

--- SampleSite/Global.asax.cs
+++ SampleSite/Global.asax.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
-using System.Data.Entity;
 
 namespace SampleSite
 {
@@ -14,6 +10,7 @@ namespace SampleSite
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
